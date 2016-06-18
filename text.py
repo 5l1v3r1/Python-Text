f6d410b630c0f2c8be8e9d2f7c339c518b99901d
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-06-18 14:47:23 +0300

    Add files via upload

---
+++ text.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+text_ico = """
+#########################################################
+#             PYTHON - TEXT - GH0ST S0FTWARE            #
+######################################################### 
+#                       CONTACT                         #
+#########################################################
+#              DEVELOPER : İSMAİL TAŞDELEN              #
+#           GMAIL : pentestdatabase@gmail.com           #
+# Linkedin : https://www.linkedin.com/in/ismailtasdelen #
+#           Whatsapp : + 90 534 295 94 31               #
+#########################################################
+"""
+
+print text_ico 
+
+star = "#########################################################"
+
+print star
+
+metin = raw_input("Bir metin giriniz : ")
+
+print star
+
+print metin
+
+print star
